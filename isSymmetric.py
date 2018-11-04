bfdcd5676329650fbc72fdfe27190a9aade5b38d
valuntiny <459201296@qq.com>
2018-11-03 20:18:51 -0400

    recursion for judging symmetric

---
+++ isSymmetric.py
@@ -0,0 +1,32 @@
+'''
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        else:
+            return self.isMirror(root.left, root.right)
+
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+
+        if left.val == right.val:
+            return self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
+        else:
+            return False
